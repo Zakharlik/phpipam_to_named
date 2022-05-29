af2416c389ae90334f37f91d74386f8ce55e96ff
Zakhar E. Likay <zlikay@kr.cs.gkovd.ru>
2022-05-29 22:43:42 +0700

    Dict reverse zones

--- dns.py
+++ dns.py
@@ -1,16 +1,56 @@
 #!/usr/bin/python3
-
-import json
+import os
+import re
 
 host_dict = {}
-with open('allip.csv', 'r', encoding='cp1251') as r:
-  for line in r:
-    net, host, _, _, name, *_ = line.split(';')
-    if host_dict.get(net) != None:
-      host_dict[net].append((host, name))
-    else:
-      host_dict[net] = [(host, name)]
-print(host_dict)
-#print(json.dumps(host_dict, sort_keys=True, indent=4))
 
-	
+data_dir = 'data'
+zones_dir = 'zones'
+
+
+def push_dict(host_dict, net, host, name):
+    if host_dict.get(net) != None:
+        host_dict[net].append((host, name))
+    else:
+        host_dict[net] = [(host, name)]
+    return host_dict
+
+
+def get_reverse_zones(host_dict, file):
+    net = re.search(r'\\(\S*)\.zone', file).group(1)
+    with open(file,'r') as f:
+        for line in f:
+            if re.search('^\d',line):
+                host, _, _, name = line.split()
+                host_dict = push_dict(host_dict, net, host, name)
+    print(host_dict)
+    return host_dict
+
+
+def get_zone(host_dict, file):
+    return host_dict
+
+
+def get_zones(host_dict, dir):
+    files = os.listdir(dir)
+    for file in files:
+        if re.search(r"^\d.*\.zone$", file):
+            host_dict = get_reverse_zones(host_dict, os.path.join(dir, file))
+        elif re.search(r".*\.zone$", file):
+            host_dict = get_zone(host_dict, file)
+    return host_dict
+
+
+def get_csv(host_dict):
+    with open('data/allip.csv', 'r', encoding='cp1251') as r:
+        for line in r:
+            net, host, _, _, name, *_ = line.split(';')
+            host_dict = push_dict(host_dict, net, host, name)
+    return host_dict
+
+
+if __name__ == '__main__':
+    host_dict = get_zones(host_dict, zones_dir)
+    host_dict = get_csv(host_dict)
+
+    print(host_dict)
